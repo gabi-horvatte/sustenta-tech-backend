c2bf7b0d63890f0429f1df1d332e887375014a9f
Gabriela Horvatte <gabrielahorvatte@gmail.com>
2025-11-16 17:04:38 -0300

    adding cors url customization

--- src/server/index.ts
+++ src/server/index.ts
@@ -18,7 +18,7 @@ export default class ExpressServer {
     const app = express();
     console.log("Express app created");
     app.use(cors({
-      origin: 'http://localhost:5173',
+      origin: process.env.FRONTEND_URL || 'http://localhost:5173',
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
       allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true,
